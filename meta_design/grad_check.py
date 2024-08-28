392c257226a381de455f3c9a8e95db1bc62abea4
Daniel Revier <revier.daniel@gmail.com>
2024-08-28 11:49:10 -0700

    New grad checker for easier extension

--- meta_design/grad_check.py
+++ meta_design/grad_check.py
@@ -1,16 +1,15 @@
-from fenics import *
-# from fenics_adjoint import *
+from optimization import OptimizationState, Epigraph, ExtremalConstraints, EnergyConstraint
+from filters import DensityFilter
+from metamaterial import Metamaterial
 import numpy as np
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import inspect
+
+import fenics as fe
 import jax
 jax.config.update("jax_enable_x64", True)
-import jax.numpy as jnp
-from matplotlib import pyplot as plt
-import matplotlib.animation as animation
-from tqdm import tqdm
-
-from metamaterial import Metamaterial
-from filters import DensityFilter
-from optimization import VolumeConstraint, IsotropicConstraint, BulkModulusConstraint, ShearModulusConstraint, OptimizationState, Epigraph, ExtremalConstraints, AndreassenOptimization, MaterialSymmetryConstraints, InvariantsConstraint, jax_density_filter, jax_projection
 
 ISQR2 = 1. / np.sqrt(2.)
 v_dict = {
@@ -18,8 +17,8 @@ v_dict = {
                       [ISQR2,  ISQR2, 0.],
                       [0.,     0.,    1.]]),
     "IBULK": np.array([[-ISQR2, ISQR2, 0.],
-                     [ ISQR2, ISQR2, 0.],
-                     [ 0.,    0.,    1.]]),
+                       [ISQR2, ISQR2, 0.],
+                       [0.,    0.,    1.]]),
     "VERT": np.array([[0., 1., 0.],
                       [1., 0., 0.],
                       [0., 0., 1.]]),
@@ -33,158 +32,118 @@ v_dict = {
                          [0., 0., 1.],
                          [1., 0., 0.]]),
     "HSA": np.array([[0.,     0.,    1.],
-                    [ISQR2, -ISQR2, 0.],
-                    [ISQR2,  ISQR2, 0.]]),
+                     [ISQR2, -ISQR2, 0.],
+                     [ISQR2,  ISQR2, 0.]]),
     "HSA2": np.array([[0.,     0.,    1.],
-                     [ISQR2,  ISQR2, 0.],
+                      [ISQR2,  ISQR2, 0.],
                       [-ISQR2, ISQR2, 0.]]),
     "IHSA": np.array([[1., 0., 0.],
                       [0., ISQR2, -ISQR2],
                       [0., ISQR2,  ISQR2]]),
     "EYE": np.eye(3),
 }
-# we have the ability to pass in an objective function.
-# we do this because on the back end only the objective actually solves the FEM problem, and then passes around the information to the constraints because none of that is going to change.
-# basically we do not need to resolve the FEM problem for every constraint
-# however, this doesn't work when we want to do a finite difference check
-# this is because we pertube the input x every time, but the constraint actually just ignore that information anyway
-# hence we have to call the objective function with the perturbed x, and then call the constraint (where the x doesn't matter)
-def finite_difference_checker(func, x, grad_analytical, epsilon=1e-5, obj=None):
+
+
+def finite_difference_checker(constraint, x, grad_analytical, params, epsilon=1e-5):
+    args_count = len(inspect.signature(constraint).parameters)
     grad_fd = np.zeros_like(grad_analytical)
-    perturb = np.zeros_like(x)
-    r_plus = np.zeros(func.n_constraints)
-    r_minus = np.zeros(func.n_constraints)
-    
-    for i in tqdm(range(grad_fd.shape[1]), desc="Checking gradient"):
+
+    for i in tqdm(range(grad_fd.shape[grad_fd.ndim-1]), desc="Checking gradient"):
+        perturb = np.zeros_like(x)
         perturb[i] = epsilon
         x_plus = x + perturb
         x_minus = x - perturb
-        
-        obj(np.zeros(obj.n_constraints), x_plus, np.array([])) if obj is not None else None
-        func(r_plus, x_plus, np.array([]))
-        
-        obj(np.zeros(obj.n_constraints), x_minus, np.array([])) if obj is not None else None
-        func(r_minus, x_minus, np.array([]))
-        
-        grad_fd[:,i] = (r_plus - r_minus) / (2. * epsilon)
-        perturb[i] = 0
 
-    diff = np.linalg.norm(grad_analytical - grad_fd)
-    fig, ax = plt.subplots(2,1, figsize=(10,10))
-    ax[0].plot(grad_analytical.flatten(), label='Analytical')
-    ax[0].plot(grad_fd.flatten(), label='Finite Difference')
-    plt.legend()
-    ax[1].plot(np.abs(grad_analytical - grad_fd).flatten()/grad_analytical.flatten(), label='Relative Difference')
-    plt.legend()
+        if params['obj']:
+            params['obj'](np.zeros(params['obj'].n_constraints),
+                          x_plus, np.array([]))
+            params['obj'](np.zeros(params['obj'].n_constraints),
+                          x_minus, np.array([]))
+
+        if args_count > 2:  # fn(results, x, grad)
+            r_plus = np.zeros(constraint.n_constraints)
+            r_minus = np.zeros(constraint.n_constraints)
+            constraint(r_plus, x_plus, np.array([]))
+            constraint(r_minus, x_minus, np.array([]))
+            grad_fd[:, i] = (r_plus - r_minus) / (2 * epsilon)
+        elif args_count == 2:  # c = fn(x, grad)
+            c_plus = constraint(x_plus, np.array([]))
+            c_minus = constraint(x_minus, np.array([]))
+            grad_fd[i] = (c_plus - c_minus) / (2 * epsilon)
+
+    plot_gradients(grad_analytical, grad_fd)
+
+
+def plot_gradients(grad_analytical, grad_fd):
+    fig, ax = plt.subplots(2, 1, figsize=(10, 10))
+    ax[0].plot(grad_analytical.flatten(), label='Analytical', marker='o')
+    ax[0].plot(grad_fd.flatten(), label='Finite Difference', marker='.')
+    ax[0].legend()
+    ax[1].plot(np.abs(grad_analytical - grad_fd).flatten() /
+               (grad_analytical.flatten()+1e-8), label='Relative Difference')
+    ax[1].legend()
     plt.show()
-    rel_diff = diff / (np.linalg.norm(grad_analytical) + np.linalg.norm(grad_fd))
 
-    # print(f"Finite Difference Gradient: {grad_fd}")
-    # print(f"Analytical Gradient: {grad_analytical}")
-    # print(f"Gradient difference: {grad_analytical - grad_fd}")
-    print(f"Absolute difference: {diff:.2e}")
-    print(f"Relative difference: {rel_diff:.2e}")
 
-    return grad_fd, diff, rel_diff
+def handle_constraints(constraint_name, x, params, epi_constraint=False):
+    constraints = {
+        'Epigraph': Epigraph(),
+        'Extremal': ExtremalConstraints(v=params['v'], extremal_mode=params['extremal_mode'], metamaterial=params['metamaterial'], ops=params['ops'], verbose=params['verbose'], plot=params['plot']),
+        'Energy': EnergyConstraint(v=params['v'], extremal_mode=params['extremal_mode'], metamaterial=params['metamaterial'], ops=params['ops'], verbose=params['verbose'], plot=params['plot']),
+    }
+
+    # if the constraint is formulated for an epigraph form we need to add a DOF for the t variable
+    if epi_constraint:
+        x = np.append(x, 0.)
+
+    constraint = constraints[constraint_name]
+    arg_nums = len(inspect.signature(constraint).parameters)
+    if arg_nums > 2:
+        grad_analytical = np.zeros((constraint.n_constraints, x.size))
+        constraint(np.zeros(constraint.n_constraints), x, grad_analytical)
+    elif arg_nums == 2:
+        grad_analytical = np.zeros(x.size)
+        constraint(x, grad_analytical)
+    else:
+        raise ValueError("Invalid number of arguments")
+    finite_difference_checker(constraint, x, grad_analytical, params)
+
 
 def main():
-    nelx = 10
+    nelx = 11
     nely = nelx
-    E_max = 1.
-    E_min = 1e-9
-    nu = 0.3
-    vol_frac = 0.5
-    start_beta, n_betas = 1, 8
-    eta = 0.5
-    pen = 3.
-    a = 2e-3
-    optim_type = 'pr'
+    E_max, E_min, nu = 1., 1e-9, 0.3
+    beta, eta = 8., 0.5
+    filter_rad = 0.1
+    meshes = {
+        'tri': fe.UnitSquareMesh(nelx, nely, 'crossed'),
+        'quad': fe.RectangleMesh.create([fe.Point(0, 0), fe.Point(1, 1)], [
+                                        nelx, nely], fe.CellType.Type.quadrilateral)
+    }
+    mesh_type = 'quad'
+    v_basis = 'BULK'
 
-    metamate = Metamaterial(E_max, E_min, nu)
-    metamate.mesh = UnitSquareMesh(nelx, nely, 'crossed')
-    # metamate.mesh = RectangleMesh.create([Point(0, 0), Point(1, 1)], [nelx, nely], CellType.Type.quadrilateral)
-    metamate.create_function_spaces()
-    
-    filt = DensityFilter(metamate.mesh, 0.1, distance_method='periodic')
-    
-    ops = OptimizationState()
-    ops.beta = start_beta
-    ops.eta = eta
-    ops.pen = pen
-    ops.filt = filt
+    # Setup parameters and initial conditions
+    params = {
+        'metamaterial': Metamaterial(E_max=E_max, E_min=E_min, nu=nu, nelx=nelx, nely=nely, mesh=meshes[mesh_type]),
+        'ops': OptimizationState(beta=beta, eta=eta),
+        'v': v_dict[v_basis],
+        'extremal_mode': 1,
+        'verbose': False,
+        'plot': False,
+        'obj': None  # Set to a constraint function if required
+    }
 
-    dim = metamate.R.dim()
-    np.random.seed(0)
-    x = np.random.uniform(1e-3, 1, dim)
-    print(f"Checking {dim:d} dimensional gradient\n")
+    params['metamaterial'].create_function_spaces()
+    params['ops'].filt = DensityFilter(
+        params['metamaterial'].mesh, filter_rad, distance_method='periodic')
+    # Initial design variables
+    x = np.random.uniform(1e-3, 1, params['metamaterial'].R.dim())
 
-    f = AndreassenOptimization(optim_type=optim_type, metamaterial=metamate, ops=ops, plot=False, verbose=False, filter_and_project=False)
-    g_vol = VolumeConstraint(V=vol_frac, ops=ops, verbose=False)
-    g_iso = IsotropicConstraint(eps=1e-5, ops=ops, verbose=False)
-    g_blk = BulkModulusConstraint(E_max, nu, a=a, ops=ops, verbose=False)
-    g_shr = ShearModulusConstraint(E_max, nu, a=a, ops=ops, verbose=False)
+    handle_constraints('Epigraph', x, params, epi_constraint=True)
+    handle_constraints('Extremal', x, params, epi_constraint=True)
+    handle_constraints('Energy', x, params)
 
-    grad_f = np.zeros_like(x)
-    grad_g_vol = np.zeros_like(x)
-    grad_g_iso = np.zeros_like(x)
-    grad_g_blk = np.zeros_like(x)
-    grad_g_shr = np.zeros_like(x)
-    
-    # compute gradients from the functions themselves
-    # f(x, grad_f)
-    # g_vol(x, grad_g_vol)
-    # g_iso(x, grad_g_iso)
-    # g_blk(x, grad_g_blk)
-    # g_shr(x, grad_g_shr)
-    
-    # Epigraph forms
-    epi = Epigraph()
-    v = v_dict['BULK']
-    g_ext = ExtremalConstraints(v=v, extremal_mode=2, metamaterial=metamate, ops=ops, verbose=False, plot=False, )
-    g_sym = MaterialSymmetryConstraints(ops=ops, eps=1e-3, symmetry_order='rectangular', verbose=False)
-    g_inv = InvariantsConstraint(ops=ops, verbose=False)
-    x = np.append(x, 1.)
-    grad_epi = np.zeros_like(x)
-    grad_extrem = np.zeros((g_ext.n_constraints, x.size))
-    grad_g_sym = np.zeros((g_sym.n_constraints, x.size))
-    grad_g_inv = np.zeros((g_inv.n_constraints, x.size))
-
-    # epi(x, grad_epi)
-    # print("Checking Epigraph Gradient:")
-    # finite_difference_checker(epi, x, grad_epi)
-
-    g_ext(np.ones(g_ext.n_constraints), x, grad_extrem)
-    print("Checking Extremal Gradient:")
-    finite_difference_checker(g_ext, x, grad_extrem)
-
-    g_inv(np.ones(g_inv.n_constraints), x, grad_g_inv)
-    print("Checking Invariants Gradient:")
-    finite_difference_checker(g_inv, x, grad_g_inv, obj=g_ext)
-    
-    # g_sym(np.ones(g_sym.n_constraints), x, grad_g_sym)
-    # print("Checking Symmetry Gradient:")
-    # finite_difference_checker(g_sym, x, grad_g_sym, obj=g_ext)
-    
-    
-    
-    
-    # Check gradients using finite differences
-    # print("Checking Objective Gradient:")
-    # finite_difference_checker(f, x, grad_f)
-    
-    # print("\nChecking Isotropic Constraint Gradient:")
-    # finite_difference_checker(g_iso, x, grad_g_iso, obj=f)
-    
-    # print("\nChecking Bulk Modulus Constraint Gradient:")
-    # finite_difference_checker(g_blk, x, grad_g_blk, obj=f)
-    
-    # print("\nChecking Shear Modulus Constraint Gradient:")
-    # finite_difference_checker(g_shr, x, grad_g_shr, obj=f)
-    
-    # print("\nChecking Volume Constraint Gradient:")
-    # finite_difference_checker(g_vol, x, grad_g_vol)
-    
-    
 
 if __name__ == "__main__":
     main()
