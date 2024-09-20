4d46be19037ecef38379b96c1933b635987793fe
Daniel Revier <revier.daniel@gmail.com>
2024-09-20 13:59:19 -0700

    Big changes to scalar

--- scripts/scalar.py
+++ scripts/scalar.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 
 import jax
 
@@ -8,130 +10,102 @@ from matplotlib import pyplot as plt
 
 from metatop import V_DICT
 from metatop.filters import jax_projection, setup_filter
-from metatop.helpers import init_density
-from metatop.mechanics import anisotropy_index, calculate_elastic_constants
+from metatop.mechanics import (anisotropy_index, calculate_elastic_constants,
+                               matrix_invariants)
 from metatop.metamaterial import setup_metamaterial
 from metatop.optimization import OptimizationState
 from metatop.optimization.scalar import EnergyConstraints, EnergyObjective
 
 np.set_printoptions(precision=5)
-# np.set_printoptions(suppress=True)
-
-RAND_SEED = 1
-print(f"Random Seed: {RAND_SEED}")
-np.random.seed(RAND_SEED)
 
 def main():
-    E_max, E_min, nu = 1., 1e-9, 0.45
-    vol_frac = 0.1
-    start_beta, n_betas = 8, 4
-    betas = [start_beta * 2 ** i for i in range(n_betas)]
-    # betas.append(betas[-1]) # repeat the last beta for final epoch when we turn on constraints
-    print(f"Betas: {betas}")
-    eta = 0.5
+    E_max, E_min, nu = 1., 1e-2, 0.45
+    start_beta, n_betas = 8, 8
     epoch_duration = 50
-    basis_v = 'VERT'
-    density_seed_type = 'uniform'
-    extremal_mode = 1
-    mesh_cell_type = 'tri'  # triangle, quadrilateral
-    if 'tri' in mesh_cell_type:
-        nelx = 50
-    elif 'quad' in mesh_cell_type:
-        nelx = 100
-        # nelx = 50
-    else:
-        raise ValueError(f"Invalid mesh_cell_type: {mesh_cell_type}")
-    nely = nelx
-
-    cell_side_length_mm = 25.
-    line_width_mm = 2.5
-    line_space_mm = line_width_mm
-    norm_line_width = line_width_mm / cell_side_length_mm
-    norm_line_space = line_space_mm / cell_side_length_mm
-    norm_filter_radius = norm_line_width
-
-    print(f"Cell Side Length: {cell_side_length_mm} mm")
-    print(f"Line Width: {line_width_mm} mm")
-    print(f"Line Space: {line_space_mm} mm")
-    print(f"Normalized Line Space: {norm_line_space}")
-    print(f"Normalized Line Width: {norm_line_width}")
-    print(f"Final Filter Radius: {norm_filter_radius}")
+    extremal_mode = 2
+    basis_v = 'BULK'
+    nelx = nely = 50
+    norm_filter_radius = 0.1
+    verbose = interim_plot = True
+    seed = 1 # 916723353 # 689993214
     # ===== End Preamble =====
 
+    np.random.seed(seed)
+
+    betas = [start_beta * 2 ** i for i in range(n_betas)]
     # ===== Component Setup =====
     metamate = setup_metamaterial(E_max,
                                   E_min,
                                   nu,
                                   nelx,
                                   nely,
-                                  mesh_cell_type=mesh_cell_type)
+                                  mesh_cell_type='tri',
+                                  domain_shape='square')
 
     filt, filt_fn = setup_filter(metamate, norm_filter_radius)
 
     # global optimization state
     ops = OptimizationState(beta=start_beta,
-                            eta=eta,
+                            eta=0.5,
                             filt=filt,
                             filt_fn = filt_fn,
                             epoch_iter_tracker=[1])
 
     # seeding the initial density
-    x = init_density(density_seed_type, vol_frac, metamate.R.dim())
-    # x = mirror_density(x, metamate.mesh)
+    x = np.random.uniform(0., 1., metamate.R.dim())
+    # x = np.random.binomial(1, 0.5, metamate.R.dim())
     # ===== End Component Setup =====
     
-    # ===== Objective and Constraints Setup =====
+    # ===== Optimizer setup ======
     v = V_DICT[basis_v]
-    f = EnergyObjective(v=v, extremal_mode=extremal_mode, metamaterial=metamate, ops=ops)
-    g = EnergyConstraints(a=2., v=v, extremal_mode=extremal_mode, ops=ops, verbose=True)
-    
-    # ===== End Objective and Constraints Setup =====
-    
-    # ===== Optimization Setup =====
+    f = EnergyObjective(v=v, 
+                        extremal_mode=extremal_mode, 
+                        metamaterial=metamate, 
+                        ops=ops,
+                        plot_interval=10,
+                        verbose=verbose,)
+
     opt = nlopt.opt(nlopt.LD_MMA, x.size)
     opt.set_min_objective(f)
-    opt.add_inequality_mconstraint(g, np.zeros(g.n_constraints))
-    # opt.add_inequality_mconstraint(g_vec, np.zeros(g_vec.n_constraints))
     
     opt.set_lower_bounds(np.zeros(x.size))
     opt.set_upper_bounds(np.ones(x.size))
-    opt.set_maxeval(epoch_duration)
+    opt.set_maxeval(2*epoch_duration)
     opt.set_param('dual_ftol_rel', 1e-6)
     # ===== End Optimization Setup =====
 
     # ===== Optimization Loop =====
-    # progressively up the projection
     for n, beta in enumerate(betas, 1):
         ops.beta, ops.epoch = beta, n
-        x[:] = np.copy(opt.optimize(x))
+        x[:] = opt.optimize(x)
+        ops.epoch_iter_tracker.append(len(f.evals))
+        
         print(f"\n===== Epoch Summary: {n} =====")
         print(f"Final Objective: {opt.last_optimum_value():.3f}")
         print(f"Result Code: {opt.last_optimize_result()}")
         print(f"===== End Epoch Summary: {n} =====\n")
-        ops.epoch_iter_tracker.append(len(f.evals))
-        
-        g.a *= 10.
 
+        opt.set_maxeval(epoch_duration)
     # ===== End Optimization Loop =====
-            
-    # ===== Post-Optimization Analysis =====
-    x = filt_fn(x)
-    x = jax_projection(x, ops.beta, ops.eta)
-    metamate.x.vector()[:] = x
-    
-    m = np.diag(np.array([1., 1., np.sqrt(2)]))
+
+    # ===== Post-Processing =====
+    metamate.x.vector()[:] = jax_projection(filt_fn(x), ops.beta, ops.eta)
+
+    m = np.diag(np.array([1, 1, np.sqrt(2)]))
     final_C = m @ np.asarray(metamate.solve()[1]) @ m
-    print('Final C:\n', final_C)
     w, v = np.linalg.eigh(final_C)
+    print('Final C:\n', final_C)
     print('Final Eigenvalues:\n', w)
     print('Final Eigenvalue Ratios:\n', w / np.max(w))
     print('Final Eigenvectors:\n', v)
 
-    print('Final ASU:', anisotropy_index(final_C, input_style='standard')[-1])
-    print('Final Elastic Constants:', calculate_elastic_constants(final_C, input_style='standard'))
-        
+    ASU = anisotropy_index(final_C, input_style='mandel')
+    elastic_constants = calculate_elastic_constants(final_C, input_style='mandel')
+    invariants = matrix_invariants(final_C)
+    print('Final ASU:', ASU)
+    print('Final Elastic Constants:', elastic_constants)
+    print('Final Invariants:', invariants)
     plt.show(block=True)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
