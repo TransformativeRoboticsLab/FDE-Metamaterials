7f5842c52db753aeb3a2cf8d0218135bb5cd5036
Daniel Revier <revier.daniel@gmail.com>
2025-03-22 03:25:07 -0700

    Fix bug if we don't give any arguments at all

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -1,5 +1,7 @@
 import argparse
 import subprocess
+import sys
+from pathlib import Path
 
 from nlopt import ForcedStop
 from utils import get_random_value, validate_param_value
@@ -25,10 +27,13 @@ def run_experiment(experiment_name, num_runs, sacred_args, random_params):
                 random_args.append(f"{param}={value}")
                 print(f"Randomized parameter '{param}' with value: {value}")
 
-        print(f"Starting run {run_id}/{num_runs if num_runs != -1 else 'infinite'}")
+        num_runs_str = 'infinity' if num_runs == -1 else str(num_runs)
+        print(f"Starting run {run_id}/{num_runs_str}")
         proc = ['python', 'experiments/' + experiment_name]
-        proc += ['with'] if len(sacred_args) == 0 else sacred_args
-        proc += random_args
+        if len(sacred_args) == 0 and len(random_args) > 0:
+            proc += ['with'] + random_args
+        else:
+            proc += sacred_args + random_args
         print(proc)
 
         try:
@@ -55,10 +60,14 @@ Note:
     """
     parser = argparse.ArgumentParser(description=description,
                                      formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('experiment_name', type=str, help='The python experiment file to run. Can use the format `experiment_name.py` or `experiment_name`.')
-    parser.add_argument('num_runs', type=int, help='The number of runs to execute. -1 for infinite runs.')
-    parser.add_argument('sacred_args', nargs='*', help="Sacred CLI style arguments to pass to the experiment (e.g., `with param1=value1 param2=value2`). Note: Parameters must be preceded by the word 'with'.")
-    parser.add_argument('--randomize', nargs='*', help="List of parameters to randomize (e.g., `--randomize param3 param4`)")
+    parser.add_argument('experiment_name', type=str,
+                        help='The python experiment file to run. Can use the format `experiment_name.py` or `experiment_name`.')
+    parser.add_argument(
+        'num_runs', type=int, help='The number of runs to execute. -1 for infinite runs.')
+    parser.add_argument('sacred_args', nargs='*',
+                        help="Sacred CLI style arguments to pass to the experiment (e.g., `with param1=value1 param2=value2`). Note: Parameters must be preceded by the word 'with'.")
+    parser.add_argument('--randomize', nargs='*',
+                        help="List of parameters to randomize (e.g., `--randomize param3 param4`)")
 
     args = parser.parse_args()
 
@@ -68,6 +77,8 @@ Note:
             try:
                 validate_param_value(param, value, verbose=False)
             except ValueError as e:
-                raise ValueError(f"Validation failed for parameter '{param}': {e}")
+                print(f"Validation failed for parameter '{param}': {e}")
+                raise
 
-    run_experiment(args.experiment_name, args.num_runs, args.sacred_args, args.randomize)
+    run_experiment(args.experiment_name, args.num_runs,
+                   args.sacred_args, args.randomize)
