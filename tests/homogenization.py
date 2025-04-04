4bedad806433c243bf070863a9b9d25e07ba8221
Daniel Revier <revier.daniel@gmail.com>
2025-04-04 15:56:50 -0700

    Add pinstripe fiber test. Results mostly agree, but disagree on isostress values. Sim values fall above the isostress lower bounds though.

--- tests/homogenization.py
+++ tests/homogenization.py
@@ -1,3 +1,4 @@
+import fenics as fe
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.testing as npt
@@ -6,17 +7,18 @@ from PIL import Image, ImageOps
 
 from metatop.image import img2func
 from metatop.mechanics import (calculate_elastic_constants,
-                               convert_isotropic_properties)
+                               convert_isotropic_properties, mandelize)
 from metatop.Metamaterial import setup_metamaterial
 
 np.set_printoptions(precision=3)
 
+# These are the base parameters we used for the optimization process
 base_config = dict(
     E_max=1.,
-    E_min=1e-9,
-    nu=0.3,
-    nelx=100,
-    nely=100,
+    E_min=1/30.,
+    nu=0.4,
+    nelx=50,
+    nely=50,
 )
 
 
@@ -32,28 +34,29 @@ def make_prop_dict(E1, E2, G12, nu12, nu21, eta122=0., eta121=0.):
     )
 
 
-def check_props(actual, desired):
-    for key in desired:
-        if key not in actual:
+def check_props(simulated, expected):
+    out = True
+    for key in expected:
+        if key not in simulated:
             logger.error(f"Key {key} not found in actual properties.")
-            return False
-        if not np.isclose(actual[key], desired[key]):
+            out = False
+        if not np.isclose(simulated[key], expected[key]):
             logger.error(
-                f"Property {key} does not match: actual={actual[key]}, desired={desired[key]}")
-            return False
-    return True
+                f"Property {key} does not match: simulated={simulated[key]:.2e}, expected={expected[key]:.2e}")
+            out = False
+    return out
 
 
 def solid_black():
+    logger.info("Testing solid black material...")
 
     metamate = setup_metamaterial(**base_config)
     metamate.x.vector()[:] = 1.
-    Chom = metamate.solve()[1]
-    sim_props = calculate_elastic_constants(Chom, input_style='standard')
+    Chom = mandelize(metamate.solve()[1])
+    sim_props = calculate_elastic_constants(Chom, input_style='mandel')
 
-    E = 1
-    nu = 0.3
-    p = convert_isotropic_properties({'E': E, 'nu': nu})
+    p = convert_isotropic_properties({'E': base_config['E_max'],
+                                      'nu': base_config['nu']})
 
     real_props = make_prop_dict(E1=p['E'],
                                 E2=p['E'],
@@ -61,10 +64,123 @@ def solid_black():
                                 nu12=p['nu'],
                                 nu21=p['nu'])
 
-    if not check_props(real_props, sim_props):
-        raise AssertionError()
+    if not check_props(sim_props, real_props):
+        logger.error("FAIL: Solid Black Material")
+    else:
+        logger.success("PASS: Solid Black Material")
 
-    logger.info("PASS: Solid Black Material")
+
+def solid_white():
+    logger.info('Testing solid white material...')
+
+    metamate = setup_metamaterial(**base_config)
+    metamate.x.vector()[:] = 0.  # solid white
+    Chom = mandelize(metamate.solve()[1])
+    sim_props = calculate_elastic_constants(Chom, input_style='mandel')
+
+    p = convert_isotropic_properties({'E': base_config['E_min'],
+                                      'nu': base_config['nu']})
+
+    real_props = make_prop_dict(E1=p['E'],
+                                E2=p['E'],
+                                G12=p['G'],
+                                nu12=p['nu'],
+                                nu21=p['nu'])
+
+    if not check_props(sim_props, real_props):
+        logger.error("FAIL: Solid White Material")
+    else:
+        logger.success("PASS: Solid White Material")
+
+
+def pinstripe_horz(vol_frac=0.5):
+    logger.info('Testing horizontal pinstripe material')
+
+    config = base_config.copy()
+    metamate = setup_metamaterial(**config)
+    dof_coords = metamate.R.tabulate_dof_coordinates()
+    high_density_dofs = np.where(dof_coords[:, 1] <= vol_frac)[0]
+    metamate.x.vector()[:] = 0.
+    metamate.x.vector()[high_density_dofs] = 1.
+
+    if not np.allclose(metamate.volume_fraction, vol_frac):
+        logger.error(
+            "Metamaterial self-tabulated volume fraction not equal to deired volume fraction. All other answers are suspect")
+
+    Chom = mandelize(metamate.solve()[1])
+    sim_props = calculate_elastic_constants(Chom, input_style='mandel')
+
+    # values for a composite based on theory
+    E_f = config['E_max']
+    E_m = config['E_min']
+    nu_f = config['nu']
+    nu_m = nu_f
+    G_f = E_f / (2 * (1 + nu_f))
+    G_m = E_m / (2 * (1 + nu_m))
+
+    E_x = vol_frac * E_f + (1 - vol_frac) * E_m
+    E_y = (vol_frac / E_f + (1 - vol_frac) / E_m)**-1
+    G_xy = (vol_frac / G_f + (1 - vol_frac) / G_m)**-1
+    nu_xy = vol_frac * nu_f + (1 - vol_frac) * nu_m
+    nu_yx = nu_xy * E_y / E_x
+
+    model_props = make_prop_dict(E1=E_x,
+                                 E2=E_y,
+                                 G12=G_xy,
+                                 nu12=nu_xy,
+                                 nu21=nu_yx)
+    print(model_props)
+    print(sim_props)
+
+    if not check_props(sim_props, model_props):
+        logger.error("FAIL: Pinstripe horizontal")
+    else:
+        logger.success("PASS: Pinstripe horizontal")
+
+
+def pinstripe_vert(vol_frac=0.5):
+    logger.info('Testing vertical pinstripe material')
+
+    config = base_config.copy()
+    metamate = setup_metamaterial(**config)
+    dof_coords = metamate.R.tabulate_dof_coordinates()
+    high_density_dofs = np.where(dof_coords[:, 0] <= vol_frac)[0]
+    metamate.x.vector()[:] = 0.
+    metamate.x.vector()[high_density_dofs] = 1.
+
+    if not np.allclose(metamate.volume_fraction, vol_frac):
+        logger.error(
+            "Metamaterial self-tabulated volume fraction not equal to deired volume fraction. All other answers are suspect")
+
+    Chom = mandelize(metamate.solve()[1])
+    sim_props = calculate_elastic_constants(Chom, input_style='mandel')
+
+    # values for a composite based on theory, sub-f is fiber, sub-m is matrix
+    E_f = config['E_max']
+    E_m = config['E_min']
+    nu_f = config['nu']
+    nu_m = nu_f
+    G_f = E_f / (2 * (1 + nu_f))
+    G_m = E_m / (2 * (1 + nu_m))
+
+    E_y = vol_frac * E_f + (1 - vol_frac) * E_m
+    E_x = (vol_frac / E_f + (1 - vol_frac) / E_m)**-1
+    G_xy = (vol_frac / G_f + (1 - vol_frac) / G_m)**-1
+    nu_yx = vol_frac * nu_f + (1 - vol_frac) * nu_m
+    nu_xy = nu_yx * E_x / E_y
+
+    model_props = make_prop_dict(E1=E_x,
+                                 E2=E_y,
+                                 G12=G_xy,
+                                 nu12=nu_xy,
+                                 nu21=nu_yx)
+    print(model_props)
+    print(sim_props)
+
+    if not check_props(sim_props, model_props):
+        logger.error("FAIL: Pinstripe vertical")
+    else:
+        logger.success("PASS: Pinstripe vertical")
 
 
 def andreassen_fig_2a():
@@ -74,6 +190,7 @@ def andreassen_fig_2a():
     Ref:
     Andreassen, E., Lazarov, B. S. & Sigmund, O. Design of manufacturable 3D extremal elastic microstructure. Mech. Mater. 69, 1-10 (2014).  
     """
+    logger.info('Testing Andreassen Fig 2a auxetic')
     # load the image, invert it because black in the image is zero value, but should be 1 in the density, then binarize it to remove any blurring/anti-aliasing effects from the inaccurate screengrab process
     img = ImageOps.invert(Image.open(
         "./tests/assets/Andreassen_Fig2a.png").convert("L"))
@@ -86,6 +203,8 @@ def andreassen_fig_2a():
     config['nely'] = 200
     # use quad mesh to try to stay true to paper's original implementation
     config['mesh_cell_type'] = 'quad'
+    config['E_min'] = 1e-9
+    config['nu'] = 0.3
     metamate = setup_metamaterial(**config)
     metamate.x = img2func(img, metamate.mesh, metamate.R)
     # metamate.plot_density()
@@ -106,11 +225,10 @@ def andreassen_fig_2a():
 
     try:
         npt.assert_array_almost_equal(Chom, published_Chom, decimal=2)
+        logger.success("PASS: Andreassen Fig. 2a double arrowhead")
     except AssertionError as e:
         logger.error(
-            f"Equivalence failed: Homogenized C with Andreassen published C: {e}")
-
-    logger.info("PASS: Andreassen Fig. 2a double arrowhead")
+            f"FAIL: Non-equivalence between homogenized C with Andreassen published C: {e}")
 
 
 def wang_table_2():
@@ -121,6 +239,7 @@ def wang_table_2():
     Wang, X., Chen, S. & Zuo, L. On Design of Mechanical Metamaterials Using Level-Set Based Topology Optimization. ASME 2015 International Design Engineering Technical Conferences and Computers and Information in Engineering Conference V02BT03A012 (2016) doi:10.1115/DETC2015-47518.
 
     """
+    logger.info('Testing Wang Table 2 material')
 
     img = ImageOps.invert(Image.open(
         "./tests/assets/Wang_Table2.png").convert("L"))
@@ -138,15 +257,17 @@ def wang_table_2():
 
     try:
         npt.assert_almost_equal(Chom, published_Chom, decimal=2)
+        logger.success("PASS: Wang Table 2")
     except AssertionError as e:
         logger.error(
             f"Equivalencve failed: Homogenized C with Wang Table 2 published C: {e}")
 
-    logger.info("PASS: Wang Table 2")
-
 
 if __name__ == "__main__":
     solid_black()
+    solid_white()
+    pinstripe_horz()
+    pinstripe_vert()
     andreassen_fig_2a()
     wang_table_2()
-    plt.show()
+    # plt.show()
